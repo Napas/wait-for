27592362f45c2988809c2f72ca15b84a6170bbf3
Mindaugas Mačiulaitis <mindaugas.maciulaitis@jdplc.com>
2020-01-13 14:21:22 +0000

    Add stderr output

--- runner/run.go
+++ runner/run.go
@@ -12,17 +12,31 @@ import (
 func Run(ctx *cli.Context, logger logrus.FieldLogger) error {
 	logger.Infof("Running %s %s", ctx.Args().First(), strings.Join(ctx.Args().Tail(), " "))
 	cmd := exec.CommandContext(ctx.Context, ctx.Args().First(), ctx.Args().Tail()...)
-	reader, err := cmd.StdoutPipe()
+	stdOutReader, err := cmd.StdoutPipe()
 
 	if err != nil {
 		return err
 	}
 
-	scanner := bufio.NewScanner(reader)
+	stdErrReader, err := cmd.StderrPipe()
+
+	if err != nil {
+		return err
+	}
+
+	stdOutScanner := bufio.NewScanner(stdOutReader)
 
 	go func() {
-		for scanner.Scan() {
-			logger.Println(scanner.Text())
+		for stdOutScanner.Scan() {
+			logger.Println(stdOutScanner.Text())
+		}
+	}()
+
+	stdErrScanner := bufio.NewScanner(stdErrReader)
+
+	go func() {
+		for stdErrScanner.Scan() {
+			logger.Println(stdErrScanner.Text())
 		}
 	}()
 
